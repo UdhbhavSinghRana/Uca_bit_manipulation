5e26df084e17f41cbbbc6b34abf4ab433d693138
Udhbhav <21889udbhav@gmail.com>
2023-09-03 22:31:14 +0530

    Minor changes

--- invert.c
+++ invert.c
@@ -11,13 +11,14 @@ void printBinary(int num) {
 int invert(int x, int p, int n) {
     int r = 1 << n;
     int l = 1 << p - 1;
-    r = r | r - 1;
+    r = r | r ;
     l = l | l - 1;
     printBinary(r);
     printBinary(l);
     r = (r - l);
     return x ^ r;
 }
+
 int main() {
     int x;
     int p;
